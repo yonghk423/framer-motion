9913ae7a2f92278ee5519cc00e33cad82341de3b
yonghk423 <yonghk423423@gmail.com>
2022-05-23 17:20:08 +0900

    start

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+`;
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Box />
+    </Wrapper>
+  );
+}
+
+export default App;
